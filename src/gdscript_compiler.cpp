ksuprynowicz/godot

Assign member type when parsing setters to prevent 'Compiler bug: unresolved assign' errors

modules/gdscript/gdscript_compiler.cpp
@@ -1067,6 +1067,7 @@ GDScriptCodeGenerator::Address GDScriptCompiler::_parse_expression(CodeGen &code
 							is_in_setter = setter_function == codegen.function_name;
 							target.mode = GDScriptCodeGenerator::Address::MEMBER;
 							target.address = codegen.script->member_indices[var_name].index;
+							target.type = codegen.script->member_indices[var_name].data_type;
 						}
 					}
 				}

SeremTitus/ruzta

src/gdscript_compiler.cpp
@@ -1094,6 +1094,7 @@ GDScriptCodeGenerator::Address GDScriptCompiler::_parse_expression(CodeGen &code
 					is_in_setter = has_setter && setter_function == codegen.function_name;
 					member.mode = GDScriptCodeGenerator::Address::MEMBER;
 					member.address = codegen.script->member_indices[var_name].index;
+					member.type = codegen.script->member_indices[var_name].data_type;
 				}
 
 				GDScriptCodeGenerator::Address target;